e7fd4b94fff135abd31fcbbae532d155c4e0fb9c
Cristi Sita <56215161+Cristi-Sita@users.noreply.github.com>
2019-11-13 15:14:57 +0200

    Add files via upload

---
+++ build/precache-manifest.8517ff787de50deb9ab5eb4906a20094.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d90c0d31ac1cd5a0d34016713b0cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11efdd1fe90afc61b981",
+    "url": "/static/css/main.719c6ae2.chunk.css"
+  },
+  {
+    "revision": "318fb1ab02dad911e985",
+    "url": "/static/js/2.25dd8399.chunk.js"
+  },
+  {
+    "revision": "11efdd1fe90afc61b981",
+    "url": "/static/js/main.d420f0f7.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "cc00747c68d09bff8a340c4baa008777",
+    "url": "/static/media/Wheather.cc00747c.jpg"
+  },
+  {
+    "revision": "5138af4bec8874b12c958aac8016b839",
+    "url": "/static/media/baloon.5138af4b.jpeg"
+  },
+  {
+    "revision": "7c770f8c63f0b0a048f50f7664941601",
+    "url": "/static/media/commercialAirplane.7c770f8c.jpg"
+  },
+  {
+    "revision": "3ac6ac987185856b6da5f6cc8e528420",
+    "url": "/static/media/commercialAirplane1.3ac6ac98.jpg"
+  },
+  {
+    "revision": "b36362c2d3d04dbe68da34629aa3edbd",
+    "url": "/static/media/commercialAirplane2.b36362c2.jpg"
+  }
+]);
